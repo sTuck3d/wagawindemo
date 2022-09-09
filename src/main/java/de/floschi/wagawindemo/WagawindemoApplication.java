51418e5e026005226e81e02e94b8a007d7cba96f
floschi <stuck3d@googlemail.com>
2022-09-09 16:52:30 +0200

    First commmit

---
+++ src/main/java/de/floschi/wagawindemo/WagawindemoApplication.java
@@ -0,0 +1,13 @@
+package de.floschi.wagawindemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WagawindemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WagawindemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/floschi/wagawindemo/WagawindemoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.floschi.wagawindemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WagawindemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
