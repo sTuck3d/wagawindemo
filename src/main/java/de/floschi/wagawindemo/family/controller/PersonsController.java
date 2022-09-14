12cf38cd7d35c26f0da12313f1a1d4109b229d3c
fschinkel <f.schinkel@web.de>
2022-09-14 12:48:58 +0200

    Removed crud

--- src/main/java/de/floschi/wagawindemo/family/controller/PersonsController.java
+++ src/main/java/de/floschi/wagawindemo/family/controller/PersonsController.java
@@ -1,12 +1,12 @@
 package de.floschi.wagawindemo.family.controller;
 
-import de.floschi.wagawindemo.family.data.PersonDto;
-import de.floschi.wagawindemo.family.data.requests.PersonRequestDto;
 import de.floschi.wagawindemo.family.data.response.ParentSummaryResponse;
 import de.floschi.wagawindemo.family.service.ParentSummaryService;
 import de.floschi.wagawindemo.family.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("persons")
@@ -22,17 +22,4 @@ public class PersonsController {
     public ParentSummaryResponse getChildInfo() {
         return parentSummaryService.loadParentSummary();
     }
-
-
-    // CRUD Operations for manual testing and development
-    @PutMapping
-    public PersonDto putPerson(@RequestBody PersonRequestDto newPersonRequest) {
-        return personService.savePerson(newPersonRequest);
-    }
-
-    @GetMapping("{personId}")
-    public PersonDto getPerson(@PathVariable Long personId) {
-        return personService.getPerson(personId);
-    }
-
 }
